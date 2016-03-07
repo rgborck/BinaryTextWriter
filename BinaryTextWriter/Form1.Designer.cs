5fc5a22d374adf389b425eaf7280db8c967c76bb
rgborck <rgborck@yahoo.com>
2016-03-07 10:32:06 -0500

    Initial commit of a working c# text [ASCII] to binary (and binary to text [ASCII]) encoder.

---
+++ BinaryTextWriter/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace BinaryTextWriter
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing) {
+      if (disposing && (components != null)) {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent() {
+      this.txtText = new System.Windows.Forms.TextBox();
+      this.txtBinary = new System.Windows.Forms.TextBox();
+      this.splitContainer1 = new System.Windows.Forms.SplitContainer();
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).BeginInit();
+      this.splitContainer1.Panel1.SuspendLayout();
+      this.splitContainer1.Panel2.SuspendLayout();
+      this.splitContainer1.SuspendLayout();
+      this.SuspendLayout();
+      // 
+      // txtText
+      // 
+      this.txtText.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.txtText.Location = new System.Drawing.Point(0, 0);
+      this.txtText.Multiline = true;
+      this.txtText.Name = "txtText";
+      this.txtText.Size = new System.Drawing.Size(422, 171);
+      this.txtText.TabIndex = 0;
+      this.txtText.TextChanged += new System.EventHandler(this.txtText_TextChanged);
+      // 
+      // txtBinary
+      // 
+      this.txtBinary.Dock = System.Windows.Forms.DockStyle.Fill;
+      this.txtBinary.Location = new System.Drawing.Point(0, 0);
+      this.txtBinary.Multiline = true;
+      this.txtBinary.Name = "txtBinary";
+      this.txtBinary.Size = new System.Drawing.Size(422, 167);
+      this.txtBinary.TabIndex = 1;
+      this.txtBinary.TextChanged += new System.EventHandler(this.txtBinary_TextChanged);
+      // 
+      // splitContainer1
+      // 
+      this.splitContainer1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.splitContainer1.Location = new System.Drawing.Point(-3, -2);
+      this.splitContainer1.Name = "splitContainer1";
+      this.splitContainer1.Orientation = System.Windows.Forms.Orientation.Horizontal;
+      // 
+      // splitContainer1.Panel1
+      // 
+      this.splitContainer1.Panel1.Controls.Add(this.txtText);
+      // 
+      // splitContainer1.Panel2
+      // 
+      this.splitContainer1.Panel2.Controls.Add(this.txtBinary);
+      this.splitContainer1.Size = new System.Drawing.Size(422, 342);
+      this.splitContainer1.SplitterDistance = 171;
+      this.splitContainer1.TabIndex = 2;
+      // 
+      // Form1
+      // 
+      this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.ClientSize = new System.Drawing.Size(419, 340);
+      this.Controls.Add(this.splitContainer1);
+      this.Name = "Form1";
+      this.Text = "Form1";
+      this.splitContainer1.Panel1.ResumeLayout(false);
+      this.splitContainer1.Panel1.PerformLayout();
+      this.splitContainer1.Panel2.ResumeLayout(false);
+      this.splitContainer1.Panel2.PerformLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.splitContainer1)).EndInit();
+      this.splitContainer1.ResumeLayout(false);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TextBox txtText;
+    private System.Windows.Forms.TextBox txtBinary;
+    private System.Windows.Forms.SplitContainer splitContainer1;
+  }
+}
+
---
+++ BinaryTextWriter/Form1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BinaryTextWriter
+{
+  public partial class Form1 : Form
+  {
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void txtText_TextChanged(object sender, EventArgs e)
+    {
+      var btext = getBinaryText(txtText.Text);
+      if (!txtBinary.Text.Equals(btext))
+        txtBinary.Text = btext;
+    }
+
+    private string getBinaryText(string input)
+    {
+      var output = new StringBuilder();
+      foreach (var c in Encoding.ASCII.GetBytes(input))
+      {
+        output.Append("0" + Convert.ToString(c, 2) + " ");
+      }
+
+      return output.ToString();
+    }
+
+    private string getTextFromBinaryString(string input)
+    {
+      var output = new List<Byte>();
+      var bytes = input.Split(new[]{' '}, StringSplitOptions.RemoveEmptyEntries);
+      foreach (var b in bytes)
+      {
+        output.Add(Convert.ToByte(b, 2));
+      }
+
+      return Encoding.ASCII.GetString(output.ToArray());
+    }
+
+    private void txtBinary_TextChanged(object sender, EventArgs e)
+    {
+      if (txtBinary.Text.Replace(" ", "").Length%8 == 0)
+      {
+        var text = getTextFromBinaryString(txtBinary.Text);
+        if (!text.Equals(txtText.Text))
+          txtText.Text = text;
+        txtBinary.SelectionStart = txtBinary.Text.Length == 0 ? 0 : txtBinary.Text.Length-1;
+        txtBinary.SelectionLength = 0;
+      }
+    }
+  }
+}
